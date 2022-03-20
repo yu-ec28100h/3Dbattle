dbad0822f9ade97291bf1501112987ce79897be0
yu-ec28100h <ec28100h@gmail.com>
2022-03-21 01:46:32 +0900

    firstcommit

---
+++ Assets/Script/Title.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    private bool firstPush = false;
+
+    public void PressStart()
+    {
+        Debug.Log("Press Start");
+
+        if(!firstPush)
+        {
+            Debug.Log("Go Stage");
+
+            SceneManager.LoadScene("CharaSelectScene");
+
+            firstPush = true;
+        }
+    }
+}
